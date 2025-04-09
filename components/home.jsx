871893c08a72a67e73a525f95b4be135450af178
888SAM-B <bsam53888@gmail.com>
2025-04-09 18:26:09 +0530

    C lang

--- components/home.jsx
+++ components/home.jsx
@@ -13,6 +13,7 @@ function home() {
 <div className="firstpage">
   <button onClick={(event)=>nav(event) } value={"java"}>JAVA</button>
   <button onClick={(event)=>nav(event) } value={"python"}>PYTHON</button>
+  <button onClick={(event)=>nav(event) } value={"c"}>C Language</button>
 </div>
 </>
 )
